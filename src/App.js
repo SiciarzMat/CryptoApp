c551360b5e41f04e98433d216aef5eadc7a469c5
Mateusz Siciarz <siciarz.mat@gmail.com>
2023-11-03 15:00:09 +0100

    Navigation done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
-import { Navbar } from "./components";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  CryptoDetails,
+  News,
+} from "./components";
+import "./App.css";
 
 const App = () => {
   return (
@@ -10,8 +18,37 @@ const App = () => {
       <div className="navbar">
         <Navbar />
       </div>
-      <div className="main"></div>
-      <div className="footer"></div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      </div>
+      <div className="footer">
+        <Typography.Title
+          level={5}
+          style={{ color: "white", textAlign: "center" }}
+        >
+          CryptoQuest <br />
+          All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to="/"> Home </Link>
+          <Link to="/exchanges"> Exchanges </Link>
+          <Link to="/news"> News </Link>
+        </Space>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import "../node_modules/antd/dist/antd";
 
 ReactDOM.render(
   <Router>
