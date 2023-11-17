df076c522885d4f68cce1cfb150f19d3c645ad54
Mateusz Siciarz <siciarz.mat@gmail.com>
2023-11-17 13:55:47 +0100

    Minor Fixes

--- src/app/store.js
+++ src/app/store.js
@@ -3,20 +3,17 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { cryptoApi } from "../services/cryptoApi";
 import { cryptoNewsApi } from "../services/cryptoNewsApi";
 import { cryptoExchangesApi } from "../services/cryptoExchangesAPI";
-import { cryptoExchangesImgApi } from "../services/cryptoExchangesImgAPI";
 
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
     [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
     [cryptoExchangesApi.reducerPath]: cryptoExchangesApi.reducer,
-    [cryptoExchangesImgApi.reducerPath]: cryptoExchangesImgApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       cryptoApi.middleware,
       cryptoNewsApi.middleware,
       cryptoExchangesApi.middleware,
-      cryptoExchangesImgApi.middleware,
     ]),
 });
